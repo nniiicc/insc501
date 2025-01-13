c50aebf003c5e529e81f1899071d76fed40447be
Nic <nmweber@uw.edu>
2025-01-13 08:34:25 -0800

    push nav update

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>INSC 501</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/nniiicc/insc501',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: https://github.com/nniiicc/insc501',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Winter 2025',
   },
 }
 
